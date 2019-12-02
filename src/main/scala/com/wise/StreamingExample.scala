c9d728a62e1f862a3a055f3f8317ec1f558c3415
Fernando Franceschi <fernando.franceschi@sumus.com.br>
2019-12-02 10:06:01 -0300

    streaming

--- src/main/scala/com/wise/StreamingExample.scala
+++ src/main/scala/com/wise/StreamingExample.scala
@@ -8,7 +8,7 @@ object StreamingExample {
   def main(args: Array[String]) = {
     val conf = new SparkConf().setMaster("local[*]").setAppName("streaming teste")
 
-    val ssc = new StreamingContext(conf, Seconds(1))
+    val ssc = new StreamingContext(conf, Seconds(10))
     val textDStream = ssc.socketTextStream("localhost", 9876)
     print(textDStream.foreachRDD(a => print(a)))
     ssc.start()
